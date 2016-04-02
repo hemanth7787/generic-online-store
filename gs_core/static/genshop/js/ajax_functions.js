aaa14026b9a962005e57b05bf9cd74f3d4d1bb0b
Hemanth Kumar <hemanth7787@gmail.com>
2016-04-02 18:17:26 +0530

    Naming conventions for javascript template objects (starts with genshop_js) Product fly to cart, using jquery UI

--- gs_core/static/genshop/js/ajax_functions.js
+++ gs_core/static/genshop/js/ajax_functions.js
@@ -1,10 +1,49 @@
 
 function acknowledge_response(param, json_obj){
-  $("#genshop_js_cart_menu").slideToggle();
-  $("#genshop_js_cart_menu").slideToggle();
+  // $("#genshop_js_cart_menu").slideToggle();
+  // $("#genshop_js_cart_menu").slideToggle();
+  var cart = $('#genshop_js_cart_menu');
+
+  var imgtodrag = $(param).parents('.product-image-wrapper').find("img").eq(0);
+  //var imgtodrag = $(this).parent('.item').find("img").eq(0);
+  if (imgtodrag) {
+      var imgclone = imgtodrag.clone()
+          .offset({
+          top: imgtodrag.offset().top,
+          left: imgtodrag.offset().left
+      }).css({
+          'opacity': '0.5',
+              'position': 'absolute',
+              'height': '191px',
+              'width': '185px',
+              'z-index': '100'
+      }).appendTo($('body'))
+          .animate({
+          'top': cart.offset().top + 10,
+              'left': cart.offset().left + 10,
+              'width': 75,
+              'height': 75
+      }, 1000, 'easeInOutExpo');
+
+      setTimeout(function () {
+          cart.effect("shake", {
+              times: 2
+          }, 200);
+      }, 1500);
+
+      imgclone.animate({
+          'width': 0,
+              'height': 0
+      }, function () {
+          $(param).detach();
+      });
+  }
 }
 
 function ajax_request(url,method,data,callback,param){
+    $.ajaxSetup({
+        data: {"csrfmiddlewaretoken": $('#genshop_js_csrf').html() },
+    });
     $.ajax({
         type:method,
         url:url,
@@ -24,7 +63,7 @@ function ajax_request(url,method,data,callback,param){
 $( ".genshop_js_basket_add" ).click(function() {
 // console.log( this.attributes["productid"].nodeValue );
 var url = $('#genshop_js_basket-add-url').html();
-var csrf = $('#genshop_js_csrf').html();
-var data = {"product_id":this.attributes["productid"].nodeValue, "csrfmiddlewaretoken":csrf}
-ajax_request(url,"POST",data, acknowledge_response,"");
+
+var data = {"product_id":this.attributes["productid"].nodeValue}
+ajax_request(url,"POST",data, acknowledge_response,this);
 });
