cf98d92d3be8f313ec8d6eb1371f02248293c0c3
Hemanth Kumar <hemanth7787@gmail.com>
2016-05-09 22:02:25 +0530

    Address forms

---
+++ shop/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from shop import models as shop_models
+
+
+class ShippingForm(forms.ModelForm):
+    pass
+
+    class Meta:
+        model = shop_models.Address
+        fields = ["title", "first_name", "last_name", "line1", "line2",
+                  "line3", "line4", "phone", "state", "postcode"]
+
+
+class BillingForm(forms.ModelForm):
+    same_as_shipping_address = forms.BooleanField()
+
+    class Meta:
+        model = shop_models.Address
+        fields = [ "same_as_shipping_address", "title", "first_name", "last_name", "line1", "line2",
+                  "line3", "line4", "phone", "state", "postcode"]
