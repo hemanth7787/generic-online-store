01cd523c8a59115730495cdd3ccf7fcfcc6a6940
Hemanth Kumar <hemanth7787@gmail.com>
2016-05-24 01:57:23 +0530

    Order model updates

--- order/models.py
+++ order/models.py
@@ -13,10 +13,12 @@ else:
 
 
 class Order(models.Model):
+    STATUS_LEVELS =  (('C', 'Created',),('P', 'Payed',))
     user = models.ForeignKey(User)
     basket = models.ForeignKey(shop_models.Basket)
-    billing_address = models.ForeignKey("BillingAddress")
+    billing_address = models.ForeignKey("BillingAddress", blank=True, null=True)
     shiping_address = models.ForeignKey("ShippingAddress")
+    bill_same_as_ship = models.BooleanField(default=False)
     order_number = models.CharField(max_length=128)
     currency = models.CharField(max_length=16)
     total_excl_tax = models.DecimalField(max_digits=8, decimal_places=2)
@@ -25,7 +27,7 @@ class Order(models.Model):
     shipping_excl_tax = models.DecimalField(max_digits=8, decimal_places=2)
     shipping_method = models.CharField(max_length=128)
     shipping_code = models.CharField(max_length=128)
-    status = models.CharField(max_length=128)
+    status = models.CharField(max_length=8, default=STATUS_LEVELS[0][0])
     guest_email = models.CharField(max_length=128)
     date_created = models.DateTimeField()
 
@@ -42,22 +44,25 @@ class ShippingAddress(models.Model):
 
 class OrderLine(models.Model):
     order = models.ForeignKey("Order")
-    vendor = models.ForeignKey(shop_models.Vendors)
     product = models.ForeignKey(shop_models.Product)
     # line_reference = models.CharField(max_length=128)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(default=1)
     price_excl_tax = models.DecimalField(max_digits=8, decimal_places=2)
     price_incl_tax = models.DecimalField(max_digits=8, decimal_places=2)
-    price_currency = models.CharField(max_length=16)
-    date_created = models.DateTimeField()
-    serial_number = models.CharField(max_length=128)
-    status_message = models.CharField(max_length=255)
-    est_date = models.DateField()
+    price_currency = models.CharField(max_length=16, null=True)
+    date_created = models.DateTimeField(default=datetime.datetime.now)
+    serial_number = models.CharField(max_length=128, null=True)
+    status_message = models.CharField(max_length=255, null=True)
+    est_date = models.DateField(null=True)
     # In case of offers
     # unit_cost_price
     # unit_price_incl_tax
     # unit_price_excl_tax
     # unit_retail_price
+    def line_total(self):
+        return self.quantity * self.price_incl_tax
+
+
 
 class Transaction(models.Model):
     order = models.ForeignKey(Order)
